1dddb9cf9978a8e67e3bae1b698e0a723b2e3798
martacasas <marta.casasp@estudiants.urv.cat>
2025-12-16 20:32:43 +0100

    Modificacions de error de FrontEnd, ara s'executa i es veu la pàgina

--- FrontEnd/src/app/components/pages/home/home.ts
+++ FrontEnd/src/app/components/pages/home/home.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { Food } from '../../../shared/models/food';
 import { FoodService } from '../../../services/food.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-
 export class Home {
-
   foods: Food[] = [];
   constructor(private foodService: FoodService) {
     this.foods = foodService.getAll();
