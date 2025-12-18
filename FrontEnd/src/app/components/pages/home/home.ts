35e084c86822345548f148a664c06c9a6d5fb9af
Fatima-Benaya <fatimabenaya41@gmail.com>
2025-12-18 18:35:27 +0100

    c

--- FrontEnd/src/app/components/pages/home/home.ts
+++ FrontEnd/src/app/components/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Food } from '../../../shared/models/food';
 import { FoodService } from '../../../services/food.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
