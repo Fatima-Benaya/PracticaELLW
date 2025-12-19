3944e68c934d38673e1bd4b8da458932700b4438
martacasas <marta.casasp@estudiants.urv.cat>
2025-12-19 11:54:49 +0100

    l

--- FrontEnd/src/app/app.ts
+++ FrontEnd/src/app/app.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 import { Header } from './components/partials/header/header';
+import { CartService } from './services/cart.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header],
+  imports: [
+    CommonModule,   // ✅ necesario para *ngIf
+    RouterOutlet,
+    Header
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {}
+export class App {
+  protected readonly title = signal('FrontEnd');
 
+  // ✅ ahora existe cartService para el HTML
+  constructor(public cartService: CartService) {}
+}
